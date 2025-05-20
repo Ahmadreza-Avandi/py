b3eab565ca5c24081c256ea07382c739730ec894
Ahmadrerza Avandi <143386424+Ahmadreza-Avandi@users.noreply.github.com>
2025-05-21 01:38:41 +0330

    اینجا همه چی درسته فقط مشکل نمایش دوربین داریم نمیدونم چرا تو تست آهر هم سالم بود

--- start.sh
+++ start.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# خروج از دایرکتوری فعلی و رفتن به دایرکتوری اصلی کاربر (معمولاً /home/pi)
-cd /home/pi
-echo "وارد دایرکتوری اصلی شدیم: $(pwd)"
-
-# تنظیم آدرس IP استاتیک
-INTERFACE="eth0"  # برای شبکه سیمی (برای وای‌فای wlan0 رو جایگزین کن)
-IP_ADDRESS="192.168.1.100"  # آدرس IP دلخواه
-NETMASK="255.255.255.0"  # ماسک زیرشبکه
-GATEWAY="192.168.1.1"  # دروازه پیش‌فرض
-
-# تنظیم آدرس IP
-sudo ip addr flush dev $INTERFACE
-sudo ip addr add $IP_ADDRESS/$NETMASK dev $INTERFACE
-sudo ip route add default via $GATEWAY
-
-# نمایش تنظیمات شبکه
-echo "تنظیمات شبکه اعمال شد:"
-ip addr show $INTERFACE
+export QT_LOGGING_RULES="qt5ct.debug=false"
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export QT_PLUGIN_PATH=""
+export QT_QPA_PLATFORM=xcb
